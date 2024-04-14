6ef1b05c5b622517351f7d1fdbeccae2e59718cf
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-04-14 21:35:06 +0700

    update feature add and fix comment

--- L3 - JSONS/index.js
+++ L3 - JSONS/index.js
@@ -134,6 +134,125 @@ app.put("/posts/:postId", async (req, res) => {
   }
 });
 
+// Viết API cho phép user được comment vào bài post.
+app.post("/posts/:id/comments", async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { content, authorId } = req.body;
+    if (!content || !authorId) {
+      throw {
+        message: "chua co noi dung comment hoac thong tin nguoi comment",
+        statusCode: 400,
+      };
+    }
+    const checkPost = await axios.get(`${API_JSON_SERVER}/posts/${postId}`);
+    if (!checkPost) {
+      throw {
+        message: "Bai viet khong ton tai",
+        statusCode: 404,
+      };
+    }
+    const checkUser = await axios.get(`${API_JSON_SERVER}/users/${authorId}`);
+    if (!checkUser) {
+      throw {
+        message: "Nguoi dung khong ton tai",
+        statusCode: 404,
+      };
+    }
+    const newComment = {
+      id: `CMT${crypto.randomUUID()}`,
+      postId,
+      content,
+      authorId,
+    };
+
+    const updatedPost = {
+      ...checkPost.data,
+      comments: [...(checkPost.data.comments || []), newComment],
+    };
+
+    const createComment = await axios.post(
+      `${API_JSON_SERVER}/comments`,
+      newComment
+    );
+
+    const updatePost = await axios.put(
+      `${API_JSON_SERVER}/posts/${checkPost.data.id}`,
+      updatedPost
+    );
+
+    res.status(201).send({
+      message: "Dang comment thanh cong",
+      data: {
+        comment: createComment.data,
+        updatedPost: updatePost.data,
+      },
+    });
+  } catch (error) {
+    res.status(error.statusCode || 500).send({
+      message: error.message || "co loi khi comment vao bai post",
+      data: null,
+    });
+  }
+});
+
+// Viết API cho phép user chỉnh sửa comment (chỉ user tạo comment mới được sửa).
+app.put("/posts/:postId/comments/:commentId", async (req, res) => {
+  try {
+    const { postId, commentId } = req.params;
+    const { content, authorId } = req.body;
+    if (!content || !authorId) {
+      throw {
+        message: "Thông tin người dùng hoặc nội dung comment không hợp lệ.",
+        statusCode: 400,
+      };
+    }
+    const checkPost = await axios.get(`${API_JSON_SERVER}/posts/${postId}`);
+    if (!checkPost || !checkPost.data) {
+      throw {
+        message: "Bài post không tồn tại.",
+        statusCode: 404,
+      };
+    }
+
+    const checkComment = await axios.get(
+      `${API_JSON_SERVER}/comments/${commentId}`
+    );
+    if (!checkComment || !checkComment.data) {
+      throw {
+        message: "Comment không tồn tại.",
+        statusCode: 404,
+      };
+    }
+    if (checkComment.data.authorId !== authorId) {
+      throw {
+        message: "Bạn không có quyền chỉnh sửa comment này.",
+        statusCode: 403,
+      };
+    }
+
+    const updatedComment = {
+      ...checkComment.data,
+      content: content,
+    };
+
+    const updateComment = await axios.put(
+      `${API_JSON_SERVER}/comments/${commentId}`,
+      updatedComment
+    );
+
+    res.status(200).send({
+      message: "Chỉnh sửa comment thành công.",
+      data: updateComment.data,
+    });
+  } catch (error) {
+    res.status(error.statusCode || 500).send({
+      message: error.message || "co loi khi sua comment",
+      data: null,
+    });
+  }
+});
+
 app.listen(8080, () => {
   console.log("Server is running!");
 });
